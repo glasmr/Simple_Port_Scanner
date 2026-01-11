7b22270acdcb2210bc9096a3989805ac600594e1
glasmr <glasmannross@gmail.com>
2026-01-11 17:17:11 -0500

    updated main

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
 mod ip_addr;
 mod parse_address;
+mod port_scan;
 
-use std::net::Ipv4Addr;
 use clap::Parser;
-use crate::ip_addr::IpAddr;
-
-#[derive(Parser)]
+use parse_address:: {
+    parse_arguments
+};
+#[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct Args {
+    #[arg(long)]
     host: String,
-    port: Option<String>,
+
+    #[arg(long)]
+    port: String,
 }
 
 fn main() {
     let args = Args::parse();
-
+    let (hosts, ports) = parse_arguments(&args);
+    dbg!(&hosts);
+    dbg!(&ports);
 
 }
