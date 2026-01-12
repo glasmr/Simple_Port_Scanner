627f4bb861ba7b96f17d6f52e0ae92f19209ca46
glasmr <glasmannross@gmail.com>
2026-01-12 10:49:29 -0500

    Updated CLI arguments

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,11 @@ mod port_scan;
 
 use clap::Parser;
 use parse_address:: {
+    ScanInfo,
     parse_arguments
 };
+use port_scan::port_scan;
+
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct Args {
@@ -14,12 +17,21 @@ pub struct Args {
 
     #[arg(long)]
     port: String,
+
+    #[arg(long, short, help = "Set timeout", default_value = "100")]
+    timeout: Option<u32>,
+
+    #[arg(long = "sS", help = "SYN scan")]
+    syn: bool,
+
+    #[arg(long = "sC", help = "Connect scan")]
+    connect: bool
 }
+
 
 fn main() {
     let args = Args::parse();
-    let (hosts, ports) = parse_arguments(&args);
-    dbg!(&hosts);
-    dbg!(&ports);
+    let mut scan_options: ScanInfo = parse_arguments(&args);
+    port_scan(&mut scan_options);
 
 }
