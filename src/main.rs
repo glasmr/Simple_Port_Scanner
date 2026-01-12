4b926fe49d33babba5c955447398433ffd316276
glasmr <glasmannross@gmail.com>
2026-01-12 14:19:05 -0500

    Changes scan type cli args

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,16 @@ mod ip_addr;
 mod parse_address;
 mod port_scan;
 
-use clap::Parser;
+use clap::{Args, Parser};
 use parse_address:: {
     ScanInfo,
     parse_arguments
 };
 use port_scan::port_scan;
 
-#[derive(Parser, Debug)]
+#[derive(Parser)]
 #[command(version, about, long_about = None)]
-pub struct Args {
+pub struct Arguments {
     #[arg(long)]
     host: String,
 
@@ -21,16 +21,26 @@ pub struct Args {
     #[arg(long, short, help = "Set timeout", default_value = "100")]
     timeout: Option<u32>,
 
-    #[arg(long = "sS", help = "SYN scan")]
+    #[command(flatten)]
+    scan_options: ScanOptions,
+}
+
+#[derive(Args)]
+#[group(multiple = false, required = false)]
+struct ScanOptions {
+    #[arg(short = 'C', long = "sC", help = "Connect scan")]
+    connect: bool,
+
+    #[arg(short = 'S', long = "sS", help = "Syn scan")]
     syn: bool,
 
-    #[arg(long = "sC", help = "Connect scan")]
-    connect: bool
+    #[arg(short = 'U', long = "sU", help = "UDP scan")]
+    udp: bool
 }
 
 
 fn main() {
-    let args = Args::parse();
+    let args = Arguments::parse();
     let mut scan_options: ScanInfo = parse_arguments(&args);
     port_scan(&mut scan_options);
 
